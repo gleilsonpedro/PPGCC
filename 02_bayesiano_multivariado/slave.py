6ec95ba3049ee2be7075d4ebe6d85c71d4113c56
gleilsonpedro <gleilsonsvo@gmail.com>
2024-04-22 20:14:55 -0300

    novamente

--- 02_bayesiano_multivariado/slave.py
+++ 02_bayesiano_multivariado/slave.py
@@ -4,40 +4,6 @@ import os
 import requests
 import zipfile
 
-
-
-# Definição da função para gerar o conjunto de dados artificial
-def generate_artificial_dataset():
-    np.random.seed(42)
-
-    # Parâmetros para a Classe 1
-    mean1 = [1, 1]
-    cov1 = [[0.1, 0], [0, 0.1]]
-    class1 = np.random.multivariate_normal(mean1, cov1, 10)
-
-    # Parâmetros para a Classe 0
-    mean2 = [0, 0]
-    cov2 = [[0.1, 0], [0, 0.1]]
-    class0_1 = np.random.multivariate_normal(mean2, cov2, 10)
-
-    mean3 = [0, 1]
-    cov3 = [[0.1, 0], [0, 0.1]]
-    class0_2 = np.random.multivariate_normal(mean3, cov3, 10)
-
-    mean4 = [1, 0]
-    cov4 = [[0.1, 0], [0, 0.1]]
-    class0_3 = np.random.multivariate_normal(mean4, cov4, 10)
-
-    class0 = np.vstack((class0_1, class0_2, class0_3))
-
-    # Combinar as classes
-    X_vertebral = np.vstack((class1, class0))
-    y_artificial = np.array([1]*10 + [0]*30)
-
-    return X_artificial, y_artificial
-
-
-
 #############################################
 # Definição da classe GaussianNB
 # Define o classificador KNN
